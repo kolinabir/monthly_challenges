3983a43bc3b7a7dff43d7b056b653d81bf3b7f09
kolinabir <knkolin9@gmail.com>
2024-02-09 23:58:24 +0600

    Update URLs and views for monthly challenges

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("january", views.index)]
+urlpatterns = [
+    path("<month>", views.monthly_challenges),
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def monthly_challenges(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "ohhh winter! Shut up"
+    elif month == "february":
+        challenge_text = "Why dont you just break one finger bro?"
+    elif month == "march":
+        challenge_text = "Shut up again for a month!"
+    else:
+        return HttpResponseNotFound("Not supported!")
+    return HttpResponse(challenge_text)
