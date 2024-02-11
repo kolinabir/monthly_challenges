87dd3cefa6542adcb6ecb89ac9bcf03cbfd41955
kolinabir <knkolin9@gmail.com>
2024-02-12 00:46:50 +0600

    Add new files and update settings.json

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
 
 # Create your views here.
 monthly_challenges_dict = {
@@ -21,13 +22,8 @@ monthly_challenges_dict = {
 def index(request):
     list_items = ""
     months = list(monthly_challenges_dict.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f'<li><a href="{month_path}">{capitalized_month}</a></li>'
 
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {"months": months})
 
 
 def monthly_challenge_by_number(request, month):
@@ -58,7 +54,11 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenges(request, month):
     try:
         challenge_text = monthly_challenges_dict[month]
-        response_data = f"<h3>{challenge_text}</h3>"
-        return HttpResponse(response_data)
+        return render(
+            request,
+            "challenges/challenge.html",
+            {"text": challenge_text, "month_name": month},
+        )
+
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
